02efb19a67c218a611a5ad118b7f3e7e0ba32cc4
Tomas Kocourek <tomas.kocourek@gthcatering.cz>
2026-01-25 19:08:40 +0100

    projistotu push

---
+++ static/pwa/serviceworker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'kliknijidlo-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/static/css/main.css',
+  '/static/js/main.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
